5e660664d9b2393228a7d5ffc1e79f056893ded2
SirNoCavalry <m.cuautle@arrendadoragob.com>
2022-12-09 13:50:30 -0600

    servidor mensaje

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => res.send("hola Mundo"));
 app.listen(4000);
-console.log("Server on port ",4000);
+console.log("Server on port ",4000);*/
+
+const net = require('net');
+const port= process.env.PORT || 4000;
+const server = net.createServer()
+
+server.on('connection', (socket)=>{
+    socket.on('data', (data)=>{
+        console.log('\nEl cliente ' + socket.remoteAddress + ":" + socket.remotePort + " dice: " + data)
+        socket.write('Recibido!')
+    })
+
+    socket.on('close', ()=>{
+        console.log('Comunicación finalizada')
+    })
+
+    socket.on('error', (err)=>{
+        console.log(err.message)
+    })
+})
+server.listen(port, ()=>{
+    console.log('servidor esta escuchando en la puerta', server.address().port)
+})
