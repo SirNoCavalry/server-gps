540e7e0c288cd3691072f06f8b8ccf6b59a0bb1c
SirNoCavalry <m.cuautle@arrendadoragob.com>
2022-12-09 16:40:20 -0600

    servidor ip

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const app = express();
-
-
+const IP = require('ip');
+app.get('/',(req, res) => {const ipAddress = IP.address();res.send(ipAddress)});
+app.listen(4000);
 console.log("Server on port ",4000);
 
 const net = require('net');
 const port= process.env.PORT || 5500;
 const server = net.createServer()
-app.get('/',(req, res) => {
-    const ipAddress = req.header('x-forwarded-for') ||  			
-						req.socket.remoteAddress;
-                        res.send(ipAddress);                    
-});
-app.listen(4000);
+
 server.on('connection', (socket)=>{
     socket.on('data', (data)=>{
         console.log('\nEl cliente ' + socket.remoteAddress + ":" + socket.remotePort + " dice: " + data)
